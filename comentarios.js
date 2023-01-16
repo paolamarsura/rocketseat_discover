4122af21b9bb7654f923bb5fbbce81bc8f0c4441
paolamarsura <paolamarsura@gmail.com>
2023-01-16 16:20:05 -0300

    Update comentarios.js

--- comentarios.js
+++ comentarios.js
@@ -76,6 +76,72 @@ O Scope ou Escopo determina a visibilidade de uma variável em um código, e par
 
 Diferentemente de var, const e let são variáveis com escopo local, ou seja, só são visíveis no escopo onde foram criadas e em escopos interiores ao de criação. Em uma variável let, porém, pode-se alterar o valor em um escopo e o valor irá persistir no escopo de criação.
 
+# Nomeando variáveis 
+
+Para nomearmos variáveis corretamente e de um jeito inteligente, precisamos saber de algumas coisas, como: 
+
+JavaScript é case-sensitive (sensível à letras maiúsculas e minúsculas) e aceita a cadeia de caracteres Unicode, podendo receber acentuações.
+Em um nome de variável em JS você pode: 
+
+- Iniciar com caracteres especiais, iniciar com letras e colocar acentos, lembrando sempre que letras maiúsculas e minúsculas fazem a diferença. 
+
+- Em contraste, você não pode: Iniciar com números e colocar espaços vazios. 
+
+- Idealmente você deve colocar nomes significativos, que fazem sentido na aplicação, explicando o que a variável é, usando camel case, onde a primeira palavra de uma frase é toda minúscula, e as subsequentes não se separam por espaço e tem a primeira letra maiúscula, por exemplo: oNomeDessaVariável, e usar nomes em inglês.
+
+##### Declaration assignment var
+
+Para declarar uma variável podemos usar var, let e const, e para atribuírmos valores à essa variável utilizamos o caractere =, e para vermos o tipo dessa variável podemos utilizar a função console.log(typeof variavel).
+
+name = "Mike"
+
+console.log (typeof name)
+
+# Agrupando declarações
+
+É possível agrupar diversas declarações de variáveis em JavaScript usando let, const ou var e colocando uma vírgula entre os nomes das variáveis a serem criadas, por exemplo: let name, age, isStudent.
+
+- Para mostrar mais de uma variável no console.log também é possível usando vírgula, por exemplo: console.log(name, age, isStudent).
+
+# Concatenando e interpolando variáveis
+
+É possível concatenar strings com o operador +, por exemplo: 
+
+console.log('o ' + name + ' tem ' + age + ' anos.') 
+
+Desta maneira as variáveis serão convertidas em texto e o que será mostrado será um texto com o nome e a idade que foram estabelecidas no código. 
+
+Uma outra maneira de escrever um texto com variáveis é por meio da interpolação com template literals, usando crase para o texto e ${} para denotar uma variável, por exemplo:
+
+console.log(O ${name} tem ${age} anos.) 
+
+O resultado deste comando será o mesmo texto do anterior.
+
+#### Criando um objeto
+
+Para criarmos um objeto utilizamos as chaves envolvendo as propriedades, que são atribuídas com dois pontos, por exemplo: 
+
+const person = { name: 'John', age: 20 }, para acessar essas propriedades usamos o ".", por exemplo: 
+console.log(person.name).
+
+#### Criando um array
+
+Para criarmos um vetor utilizamos [ ] envolvendo os valores, por exemplo:
+
+const animals = [ "Lion", "Monkey" ], para acessar esses valores usamos a posição do valor no vetor, começando por 0, por exemplo: console.log(animals[0]). 
+
+Para descobrirmos o número de posições de um vetor qualquer, podemos utilizar a propriedade length, por exemplo: animals.length.
+
+
+
+
+
+
+
+
+
+
+
 
 
 
